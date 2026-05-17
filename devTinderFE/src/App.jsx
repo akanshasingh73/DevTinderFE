22c1e2a413d7d5606a349e3d92c9c9e259f1aa74
akanshasingh73 <akanshasingh73@gmail.com>
2026-05-17 22:34:55 +0530

    created store and store user data from profile

--- devTinderFE/src/App.jsx
+++ devTinderFE/src/App.jsx
@@ -1,19 +1,26 @@
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 import Body from './components/Body';
+import Feed from './components/Feed';
 import Login from './components/Login';
 import Profile from './components/Profile';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Body />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/profile' element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Body />}>
+              <Route path='/login' element={<Login />} />
+              <Route path='/feed' element={<Feed />} />
+              <Route path='/profile' element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
